2059835c4c429461841571b690930a049ad935f8
luan vidal <luanvidal6565@outlook.com>
2024-10-26 16:40:43 -0300

    feat: add tailwind css extend theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function PageHome() {
   return (
-    <main>
+    <main className="">
       Home
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: 'var(--color-text)',
+        error: 'var(--color-error)',
+        primary: 'var(--color-primary)',
+        background: 'var(--color-background)',
+        'primary-context': 'var(--color-primary-context)',
+
+      }
+    },
   },
   plugins: [],
 };
